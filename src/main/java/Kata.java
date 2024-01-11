22e78913bf0fec8cfc873d5ec2147b10fb8092eb
nicolas.romanzin <nicolas.romanzin@thalesgroup.com>
2024-01-11 10:46:50 +0100

    add testing dependencies and placeholder classes and tests

---
+++ src/main/java/Kata.java
@@ -0,0 +1,5 @@
+public class Kata {
+    public static void main(String[] args) {
+        System.out.println("Welcome to the Dojo");
+    }
+}
---
+++ src/test/java/KataTest.java
@@ -0,0 +1,10 @@
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class KataTest {
+    @Test
+    public void shouldTestSomeBehavior() {
+        assertEquals(4, 2 + 2);
+    }
+}
