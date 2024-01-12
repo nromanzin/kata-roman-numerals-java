f2fddcc59d04312db53cf12254efa9d72d66682d
nicolas.romanzin <nicolas.romanzin@thalesgroup.com>
2024-01-12 10:15:20 +0100

    works up to 3

--- src/main/java/Converter.java
+++ src/main/java/Converter.java
@@ -1,5 +1,20 @@
+import java.util.Map;
+
 public class Converter {
+
+    private Map<Integer, String> conversionMap = Map.of(
+            10, "X",
+            9, "IX",
+            5, "V",
+            4, "IV",
+            1, "I"
+    );
+
     public String toRoman(int number) {
-        return "I";
+        StringBuilder roman = new StringBuilder();
+        for (int i = 0; i < number; i++) {
+            roman.append("I");
+        }
+        return roman.toString();
     }
 }
--- src/test/java/ConverterTest.java
+++ src/test/java/ConverterTest.java
@@ -15,5 +15,15 @@ public class ConverterTest {
     public void shouldReturnIWhenGiven1() {
         assertEquals("I", converter.toRoman(1));
     }
+
+    @Test
+    public void shouldReturnIIWhenGiven2() {
+        assertEquals("II", converter.toRoman(2));
+    }
+
+    @Test
+    public void shouldReturnIIIWhenGiven3() {
+        assertEquals("III", converter.toRoman(3));
+    }
 }
 
